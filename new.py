eeb44f3834e775557b7f3a139151ccc89175554c
Lekshmy <lekshmys33@gmail.com>
2026-09-01 21:58:20 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import imageDataGenerator 
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense,Dropout, Flatten
+from tensdorflow.keras.callbacks import EarlyStopping
+
+
+train_path="data/train"
+test_path="data/test"
+
+
+train_data=imageDataGenerator(rescale=1./255,rotation_range=30,zoom_range=0.2,horizontal_flip=True)
+test_data=imageDataGenerator(rescale=1./255)
+
